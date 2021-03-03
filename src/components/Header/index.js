d75d55b4a75fbc78a8ffa241621c14f0131ebcf4
Vanessa Limana <vanessa4562008@hotmail.com>
2021-03-03 11:34:06 -0300

    Construção do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react"; 
+import './header.css'
+import {link} from 'react-router-dom';
 
 class Header extends Component{
     render(){
         return(
-            <div>
-                <strong>
-                    Fimaria
-                </strong>
+            <div className="header">
+                <Link to="/">Fimaria</Link>
             </div>
         );
     }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {BrowseRouter, Route, Switch} from 'react-router-dom';
-
-import Home from './pages/Home';
+import Header from './components/Header';
+//import Home from './pages/Home';
 
 const Routes = () => {
     return(
         <BrowseRouter>
+        <Header />
             <Switch>
                 <Route exact path ="/" component={Home} />
             </Switch>
